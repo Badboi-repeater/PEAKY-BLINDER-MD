c4391d42ed5b76e4da0833d628fe9ace0a9f5614
Badboi-repeater <oluwamighty736@gmail.com>
2026-04-29 00:32:34 +0100

    Update owner details in setting.js

--- setting.js
+++ setting.js
@@ -4,9 +4,9 @@
 
 module.exports = {
   BOT_NAME: process.env.BOT_NAME || 'ᴘᴇᴀᴋʏ-ʙʟɪɴᴅᴇʀ-ᴍᴅ', 
-  OWNER_NAME: process.env.OWNER_NAME || 'Mr Lee', 
-  OWNER_NUMBER: process.env.OWNER_NUMBER || '254116266407', 
-  SESSION_ID: process.env.SESSION_ID || '', // paste here your blinders session ID
+  OWNER_NAME: process.env.OWNER_NAME || 'Boy Gentle', 
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '2349066853654', 
+  SESSION_ID: process.env.SESSION_ID || '', //  blinder~a7JGQAaQ#N43PkniOBnKncJ24vtocKk13yoiY2f2h-ByU81-DVhQ
   MODE: process.env.MODE || "private", // public or private
   TIMEZONE: process.env.TIMEZONE || 'Africa/Nairobi' 
 };
